9fc72e0852264941ffc92c86d44fe331d15f2f3c
drusjs <magnojoky962@gmail.com>
2024-04-23 21:09:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+if (document.querySelector('.faq__button')) {
+    document.querySelectorAll('.faq__button').forEach((el) => {
+        el.addEventListener('click', (e) => {
+            e.currentTarget.parentElement.classList.toggle('active')
+        })
+    })
+}
+
+
+let pageUpButton = document.querySelector('.page-up__button')
+if (pageUpButton) {
+    pageUpButton.addEventListener('click', () => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        });
+    })
+}
+window.addEventListener('scroll', () => {
+    console.log()
+    if (+window.scrollY >= 600) {
+        pageUpButton.classList.remove('hide')
+    } else {
+        pageUpButton.classList.add('hide')
+    }
+})
+
+if (document.querySelector('.burger-button')) {
+    document.querySelector('.burger-button').addEventListener('click', (e) => {
+        document.querySelector('.nav').classList.toggle('active')
+        e.currentTarget.classList.toggle('active')
+    })
+}
